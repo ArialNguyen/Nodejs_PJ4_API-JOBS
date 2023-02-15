bf93046173bc5c45edc34811e23803fff669f1c0
ArialNguyen <Hungnguyen100802@gmail.com>
2023-02-16 00:12:56 +0700

    delete All

--- errors/AllError.js
+++
@@ -1,5 +0,0 @@
-const BadRequest = require("./BadRequest")
-const CustomApiError = require("./CustomApiError")
-const Unauthentication = require("./Unauthentication")
-
-module.exports = {BadRequest, CustomApiError, Unauthentication}
